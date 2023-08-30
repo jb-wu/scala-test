1526e9663614374ce3b42a7f722137793c55e109
wjb <wjb@163.com>
2023-08-30 14:33:02 +0800

    first commit

---
+++ src/main/scala/com/wjb/scala/HelloGit.scala
@@ -0,0 +1,14 @@
+package com.wjb.scala
+
+/**
+  * Author: wjb
+  * Date: 2023-08-30-14:31
+  * Desc:
+  */
+object HelloGit {
+
+  def main(args: Array[String]): Unit = {
+    println("hello git master 1")
+  }
+
+}
